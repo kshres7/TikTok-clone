2deb50b591b5d792e50c4a4108e372823afc1524
Karan Shrestha <karanshrestha96@gmail.com>
2020-10-04 22:54:38 -0400

    added the snap functionality to switch videos

---
+++ src/Video.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Video.css';
+
+function Video() {
+  return (
+    <div className="video">
+      <video
+      className="video__player"
+      loop
+      src="https://v19-web-newkey.tiktokcdn.com/23961d56027d983c8550471a62101c1c/5f7ad8b6/video/tos/useast2a/tos-useast2a-ve-0068c003/278a6930ceec44d795a2e0cd60a94861/?a=1988&amp;br=3128&amp;bt=1564&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;l=2020100502262001019021922447ABD5B2&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;qs=0&amp;rc=amZubHN5a3FvdjMzPDczM0ApNTUzZmY1MztmN2c8NmU0OWcuMWMuaWNgZ2NfLS1jMTZzcy4zLjQ2NmExNDZgYWNeYGA6Yw%3D%3D&amp;vl=&amp;vr="></video>
+    </div>
+  );
+}
+
+export default Video;
